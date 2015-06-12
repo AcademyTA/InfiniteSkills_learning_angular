f525172f8f4069397e29b5e713d5d0991545ab0b
Jason <cameljeet@gmail.com>
2015-06-11 23:38:31 -0700

    2.04 Using ng-repeat to list information

--- serving_angularjs/app.js
+++ serving_angularjs/app.js
@@ -1,12 +1,7 @@
 var app = angular.module('app', ['controllers']);
 
 angular.module('controllers', []).controller('MainController', function($scope) {
-  $scope.val = 1
-  $scope.even = false
-  
-  $scope.inc = function(){
-    $scope.val += 1
-    $scope.even = $scope.val % 2 == 0
-  }
-  
+  $scope.myarr = [1,2,3,4,5,5] // the extra 5 will break the code, need to add $index
+  $scope.users = [{name: "Kyle", age: 28}, {name: "Demar", age: 26}, {name: "Jonas", age: 25}, {name: "Greivis", age: 29}]
+  $scope.ob = {name: "Michael", age:50, position:"Shooting Guard"}
 })
