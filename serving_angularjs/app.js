feeaf7854eb9b5119da3aa51a8905adc8cb4318c
Jason <cameljeet@gmail.com>
2015-06-12 00:06:56 -0700

    2.05 Using ng-class, ng-include and ng-template

--- serving_angularjs/app.js
+++ serving_angularjs/app.js
@@ -1,7 +1,5 @@
 var app = angular.module('app', ['controllers']);
 
 angular.module('controllers', []).controller('MainController', function($scope) {
-  $scope.myarr = [1,2,3,4,5,5] // the extra 5 will break the code, need to add $index
-  $scope.users = [{name: "Kyle", age: 28}, {name: "Demar", age: 26}, {name: "Jonas", age: 25}, {name: "Greivis", age: 29}]
-  $scope.ob = {name: "Michael", age:50, position:"Shooting Guard"}
+  $scope.classVar = true
 })
