9316fb42b1071f69001fd6f8af9a8addc77c67ff
Jason <cameljeet@gmail.com>
2015-06-12 16:20:12 -0700

    3.02 More built in filters

--- serving_angularjs/app.js
+++ serving_angularjs/app.js
@@ -1,7 +1,10 @@
 var app = angular.module('app', []);
 
-app.controller('MainController', function($scope) {
-  $scope.mydata = {arr: ["Zach", "Raul", "Martine", "Zed", "Rudy", "Moesha", "Avdi"]}
+app.controller('MainController', function($scope, $filter) {
+
   $scope.myobjectdata = {arr: [{name: "Zach", age: 18}, {name: "Raul", age: 25}, {name: "Martine", age: 69}, {name: "Zed", age: 23}, {name: "Rudy", age: 32}, {name: "Moesha", age: 21}, {name: "Avdi", age: 44}]}
+
+  console.log($filter('uppercase')('make this uppercase!'))
+
 }) 
 
