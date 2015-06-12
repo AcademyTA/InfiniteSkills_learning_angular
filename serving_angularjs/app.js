411d4af608b385a27a83daee7df90c3735bb4f95
Jason <cameljeet@gmail.com>
2015-06-11 19:05:09 -0700

    2.03 Added increment button and even/odd toggle

--- serving_angularjs/app.js
+++ serving_angularjs/app.js
@@ -1,5 +1,12 @@
 var app = angular.module('app', ['controllers']);
 
 angular.module('controllers', []).controller('MainController', function($scope) {
-  $scope.val = "test&*("
+  $scope.val = 1
+  $scope.even = false
+  
+  $scope.inc = function(){
+    $scope.val += 1
+    $scope.even = $scope.val % 2 == 0
+  }
+  
 })
