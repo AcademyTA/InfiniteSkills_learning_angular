fe6cb0da368bf4ee9c20e3c4cd9ee58b588d64f0
Jason <cameljeet@gmail.com>
2015-06-13 13:34:29 -0700

    4.04 Services

--- serving_angularjs/app.js
+++ serving_angularjs/app.js
@@ -1,17 +1,53 @@
 var app = angular.module('app', []);
 
-app.controller('MainController', function($scope,myFactory) {
-  console.log(myFactory.getdata())
-  console.log(myFactory.mydata)
-  // cannot access mydata, only the getdata() function can.
+app.controller('MainController', function($scope, myFactory, myService) {
+  // console.log(myFactory.getData())
+  // myFactory.addData('bla bla bla')
+  // console.log(myFactory.getData())
+  console.log(myService.getData())
+  myService.addData('bla bla bla')
+  console.log(myService.getData())
 })
 
-app.factory('myFactory',function() {
-  var mydata = "this is some other data"
-  var myfunc = function() {}
+app.factory('myFactory', function() {
+  var myString = "this is some other data"
+  var addToString = function(newstr) {
+    myString += newstr
+  }
   return {
-    getdata: function() {
-      return mydata
-    }  
+    getData: function() {
+      return "String contains: " + myString
+    },
+    addData: addToString
   }
 })
+
+// app.service("myService", function(){
+//   var myString = "this is some other data"
+//   var addToString = function(newstr) {
+//     myString += newstr
+//   }
+
+//   this.getData = function() {
+//     return "String contains: " + myString
+//   },
+//   this.addData = addToString
+// })
+
+// app.factory('myService', function(){
+//   return new ServiceClass()
+// })
+
+app.service('myService', ServiceClass)
+
+function ServiceClass() {
+  var myString = "this is some other data"
+  var addToString = function(newstr) {
+    myString += newstr
+  }
+
+  this.getData = function() {
+    return "String contains: " + myString
+  },
+  this.addData = addToString
+}
