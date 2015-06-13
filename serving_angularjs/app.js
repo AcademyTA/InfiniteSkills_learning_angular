b306dfd6efd1504875c1fa4247bd71a56fd29147
Jason <cameljeet@gmail.com>
2015-06-13 00:07:48 -0700

    3.03 Custom Filters

--- serving_angularjs/app.js
+++ serving_angularjs/app.js
@@ -4,7 +4,36 @@ app.controller('MainController', function($scope, $filter) {
 
   $scope.myobjectdata = {arr: [{name: "Zach", age: 18}, {name: "Raul", age: 25}, {name: "Martine", age: 69}, {name: "Zed", age: 23}, {name: "Rudy", age: 32}, {name: "Moesha", age: 21}, {name: "Avdi", age: 44}]}
 
-  console.log($filter('uppercase')('make this uppercase!'))
-
 }) 
 
+app.filter('charlimit', function(){
+  return function(input, length){
+    if(!length){
+      length = 10
+    }
+    if(!input){
+      imput = ""
+    }
+    if(input.length <= length){
+      return length
+    } else {
+      return input.substring(0,length) + "..."
+    }
+  }
+})
+
+app.filter('candrink', function(){
+  return function(data, minage){
+    filtered = []
+    if(!minage){
+      minage = 21
+    }
+    for(var i = 0; i < data.length; i++){
+      var value = data[i]
+      if(value.age > minage){
+        filtered.push(value)
+      }
+    }
+    return filtered
+  }
+})
